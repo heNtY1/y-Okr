d2bad9f9be3a1f75f47c5a0c101b7ea496fec6e0
heNtY1 <zuravlevnikita78@gmail.com>
2025-01-16 00:02:33 +0300

    ok

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import sys
 from random import randint
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-class Git(QMainWindow):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(568, 424)
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 81, 31))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "start"))
+
+
+class Git(Ui_Form, QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.initUI()
         self.flag = False
         self.sps = []
 
     def initUI(self):
-        uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.run)
 
     def run(self):
@@ -31,13 +48,16 @@ class Git(QMainWindow):
     def draw_flag(self, qp):
         x, y = randint(0, 560), randint(0, 420)
         r = randint(0, 200)
-        self.sps.append((x, y, r))
-        qp.setBrush(QColor(255, 255, 0))
+        h, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+        self.sps.append((x, y, r, h, g, b))
         for i in self.sps:
+            qp.setBrush(QColor(i[3], i[4], i[5]))
             qp.drawEllipse(i[0], i[1], i[2], i[2])
+
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
